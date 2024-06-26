b9eea0024804975e4a8312941e91964345069162
bagussurya12 <bagus.suryaindrajati@gmail.com>
2024-06-26 14:59:24 +0700

    Update Card Profile

--- components/CardProfile.vue
+++ components/CardProfile.vue
@@ -141,7 +141,7 @@
                 </ul>
                 <div class="flex flex-wrap">
                   <a
-                    href="https://wa.me/yourphonenumber"
+                    href="`https://wa.me/${profile.socialmedia.tiktok}`"
                     target="_blank"
                     class="mt-10 rounded-full bg-green-700 py-2 px-4 text-white font-semibold hover:bg-green-600 flex items-center text-xl"
                   >
